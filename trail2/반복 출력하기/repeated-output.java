327e0d137952ac8dabed8ad156987be5d18eb290
SamK5678 <142370028+samkkkkkkk@users.noreply.github.com>
2026-05-16 15:17:04 +0900

    [Passed] 반복 출력하기 (132ms, 10MB)

---
+++ trail2/반복 출력하기/repeated-output.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void printString(int N){
+        for(int i=0; i < N; i++) {
+            System.out.println("12345^&*()_");
+        }
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int rowNum= sc.nextInt();
+        printString(rowNum);
+    }
+}
